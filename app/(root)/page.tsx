8828f789216546cbc454bbbc0c6aab5da57a3497
Широков Глеб <gotlibpro@yandex.ru>
2025-05-23 10:18:15 +0500

    added post layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,44 @@
 import SearchForm from "@/components/SearchForm";
+import ArticleCard from "@/components/ArticleCard";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?:string }>
 }) {
   const query = (await searchParams).query;
 
+  const posts = [{
+    id: 1,
+    _createdAt: new Date(),
+    views: 35,
+    author: { _id: 1, name: "Shirokov Gleb" },
+    description: "description",
+    image: "https://play-lh.googleusercontent.com/lmG9HlI0awHie0cyBieWXeNjpyXvHPwDBb8MNOVIyp0P8VEh95AiBHtUZSDVR3HLe3A=w240-h480-rw",
+    category: "ai",
+    title: "New ChatGPT device"
+  }]
+
   return (
     <>
       <section className="dark-container">
         <h1 className="heading">Главная страница</h1>
 
-        <p className="sub-heading">Добавляйте новую информацию легко</p>
+        <p className="sub-heading">Делитесь своими идеями!</p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          { query ? `Результаты поиска по "${query}"` : 'Все статьи' }
+        </p>
+
+        <ul className="mt-7 card_grid">
+          { posts.length > 0 ? posts.map((post: ArticleTypeCard) => (
+            <ArticleCard key={post?.id} post={post} />
+          )) : (
+            <p className="no-results">Статей не найдено</p>
+          )}
+        </ul>
       </section>
     </>
   );
---
+++ components/ArticleCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {formatDate} from "@/lib/utils";
+import {EyeIcon} from "lucide-react";
+import Link from "next/link";
+
+function ArticleCard({ post }: { post: ArticleTypeCard }) {
+  return (
+    <li className="article-card group">
+      <div className="flex-between">
+        <p className="article_card_date">
+          {formatDate(post._createdAt)}
+        </p>
+
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-16-medium">{post.views}</span>
+        </div>
+
+        <div className="flex-between mt-5 gap-5">
+          <div className="flex-1">
+            <Link href={`/user/${post.author?._id}`}>
+              <p className="text-16-medium line-clamp-1">
+                {post.author?.name}
+              </p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default ArticleCard;
